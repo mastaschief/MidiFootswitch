c39528bed6befafc597da06f38c94cecba54b5b9
mastaschief <40563859+mastaschief@users.noreply.github.com>
2023-05-12 10:04:28 +0200

    Update midiFootswitch.ino

--- midiFootswitch.ino
+++ midiFootswitch.ino
@@ -6,7 +6,6 @@
 #define BUTTON1PIN 2
 #define LED1PIN 3
 
-
 uint8_t buttons[] = {BUTTON1PIN}; // Add BUTTONXPIN to list
 
 uint8_t leds[] = {LED1PIN}; // Add LEDXPIN to list
@@ -15,34 +14,31 @@ bool states[sizeof(buttons)] = {};
 
 long debounce[sizeof(buttons)] = {}; // Array to debounce the buttons
 
-
 void controlChange(byte channel, byte control, byte value)
 {
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
 }
 
-
-void sendMidiMessage(uint8_t button, bool state) // button is the BUTTONXPIN, state is the current state of the switch
+void sendMidiMessage(uint8_t ccCommand, bool state) // ccCommand is the desired cc , state is the current state of the switch
 {
   if (state)
   {
-    controlChange(button, 10, 127); // Turn On
+    controlChange(ccCommand, 10, 127); // Turn On
   }
   else
   {
-    controlChange(button, 10, 1); // Turn Off
+    controlChange(ccCommand, 10, 1); // Turn Off
   }
-  MidiUSB.flush();// Send Midi Message
+  MidiUSB.flush(); // Send Midi Message
 }
-
 
 void setup()
 {
   for (size_t i = 0; i < sizeof(buttons); i++) // set pinModes for buttons and leds
   {
-  pinMode(buttons[i], INPUT_PULLUP);
-  pinMode(leds[i], OUTPUT);
+    pinMode(buttons[i], INPUT_PULLUP);
+    pinMode(leds[i], OUTPUT);
   }
 
   for (size_t i = 0; i < sizeof(buttons); i++) // Set every state off so no effect will be activated by startup
@@ -52,7 +48,6 @@ void setup()
   }
 }
 
-
 void loop()
 {
   for (size_t i = 0; i < sizeof(buttons); i++)
@@ -61,10 +56,11 @@ void loop()
     {
       if (!digitalRead(buttons[i])) // read if button is pressed
       {
-        states[i] = !states[i]; // change state
-        sendMidiMessage(states[i], i); // Send Midi Message
-        digitalWrite(leds[i], states[i]); // Set LED
-        debounce[i] = millis(); // Debounce
+        states[i] = !states[i];                // change state
+        uint8_t ccMessage = i + 1;             // can't be 0
+        sendMidiMessage(ccMessage, states[i]); // send midi CC message
+        digitalWrite(leds[i], states[i]);      // Set LED
+        debounce[i] = millis();                // Debounce
       }
     }
   }
